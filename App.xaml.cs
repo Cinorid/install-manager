5c47da172bed652bdacd46d9c40517cb07dea843
ddrake <ddrake@7caf2849-a630-4387-b67e-cc3086ce7676>
2011-09-16 13:07:57 +0000

    Multiple Architecture support added. This fixes feature #35.
    git-svn-id: https://projects.fhtc.edu/svn/exscripts/trunk@7 7caf2849-a630-4387-b67e-cc3086ce7676

--- App.xaml.cs
+++ App.xaml.cs
@@ -13,8 +13,14 @@ namespace JenzabarSilentInstall {
 
     protected override void OnStartup(StartupEventArgs e) {
 
-      if (e.Args != null && e.Args.Count() > 0) {
-        this.Properties["DBConnection"] = e.Args[0];
+      this.Properties["ConfigurationPath"] = "";
+
+      if (e.Args != null && e.Args.Count() > 0 && e.Args.Count() % 2 == 0) {
+
+        for (int i = 0; i < e.Args.Count(); i += 2) {
+          if (e.Args[i] == "-c" || e.Args[i] == "--config") { this.Properties["ConfigurationPath"] = e.Args[i + 1]; }
+        }
+
       }
 
       base.OnStartup(e);
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,6 +15,8 @@ using Microsoft.Win32;
 using System.Diagnostics;
 using System.Management.Automation;
 using System.IO;
+using System.Xml;
+using System.Xml.XPath;
 
 namespace JenzabarSilentInstall {
   /// <summary>
@@ -22,25 +24,40 @@ namespace JenzabarSilentInstall {
   /// </summary>
   public partial class MainWindow : Window {
 
+    private XmlDocument xmlDocument = new XmlDocument();
+
+    private string ConfigurationPath { get; set; }
+
+    private List<string> ClientList = new List<string>();
+
     public MainWindow() {
       InitializeComponent();
 
-      if (Application.Current.Properties["DBConnection"] != null) {
-        this.textBox1.Text = Application.Current.Properties["DBConnection"] + "\r\n";
+      this.ConfigurationPath = Application.Current.Properties["ConfigurationPath"] != null ? Application.Current.Properties["ConfigurationPath"].ToString() : Environment.CurrentDirectory + @"\config\";
+      this.ConfigurationPath = Directory.Exists(ConfigurationPath) ? ConfigurationPath : Environment.CurrentDirectory + @"\config\";
+      this.ConfigurationPath += ConfigurationPath.EndsWith(@"\") ? "" : @"\";
+      this.ConfigurationPath += Environment.Is64BitOperatingSystem ? "config-x64.xml" : "config-x86.xml";
+
+      if (File.Exists(ConfigurationPath)) {
+        using (StreamReader sr = new StreamReader(ConfigurationPath)) {
+          xmlDocument.LoadXml(sr.ReadToEnd());
+          sr.Close();
+        }
       }
 
-      //RegistryKey key = Registry.LocalMachine.OpenSubKey("SOFTWARE");
+      this.lblMacNameDescription.Content = Environment.MachineName;
+      this.lblOSDescription.Content = String.Format("{0} 64 bit support: {1}", Environment.OSVersion, Environment.Is64BitOperatingSystem ? "Yes" : "No");
 
-      List<string> thiss = new List<string>();
-      // List<String> thiss = new ArrayList<string>();
+      foreach (XmlNode n in xmlDocument.DocumentElement.SelectNodes("ClientList/name")) {
+        this.ClientList.Add(n.InnerText.Trim());
+      }
 
-      //foreach (String s in key.GetSubKeyNames()) {
-        //this.textBox1.Text += s + "\r\n";
-        string strBaseInstall = @"\\hadbsvc\Jenzabar\EX\Client_Installs\3.6\EX-3.6-Setup.exe /S /V";
-        string strBaseOptions = @"\\hadbsvc\Jenzabar\Script_Files\EX36Setup.txt";
-        string strBaseSwitches = " /qn SETUPFILE=";
-        this.textBox1.Text = String.Format("{0} \"{1}\" {2}", strBaseInstall, strBaseSwitches, strBaseOptions);
+      if (this.ClientList.Contains(Environment.MachineName)) {
 
+      }
+      else {
+        Application.Current.Shutdown(0);
+      }
        
 
         //if (File.Exists("config/sample.txt")) {
