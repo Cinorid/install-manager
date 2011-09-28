6bbb0ffbb5e9e8380be1f7b51bcd46ddb2aa66d0
aminor <aminor@7caf2849-a630-4387-b67e-cc3086ce7676>
2011-09-28 17:58:10 +0000

    git-svn-id: https://projects.fhtc.edu/svn/exscripts/trunk@25 7caf2849-a630-4387-b67e-cc3086ce7676

--- PreRequisites.ps1
+++ PreRequisites.ps1
@@ -1,26 +1,30 @@
 ﻿cls
-
+$env:SEE_MASK_NOZONECHECKS = 1
 $Clients = New-Object Collections.Generic.List[string];
 $xmlDoc = New-Object Xml.XmlDocument;
-
 try {
   $loc = Get-Location;
   $xmlDoc.Load("$loc\ClientList.xml");
+  #$xmlDoc.Load("C:\Users\aminor\Desktop\Jenzabar-PowerFaids Maintenance\trunk\config\ClientList.xml");
   $machineName = [System.Environment]::MachineName;
   
   foreach ($node in $xmlDoc.DocumentElement.SelectNodes("clients/name")) {
     $Clients.Add($node.InnerText.Trim().ToUpper());
   }
   
-  if ($Clients.Contains($machineName)) {
-    Write-Host "Good";
+  if ($Clients.Contains($machineName))
+  {
   }
+  Else
+  	{
+		Remove-Item env:\SEE_MASK_NOZONECHECKS
+		Exit
+	}
 }
 catch {
 
 }
 
-$env:SEE_MASK_NOZONECHECKS = 1
 $WinInst45 = "\\hadbsvc.fhtc.lan\Jenzabar\SQL2008\x86\redist\Windows Installer\x86\INSTMSI45XP.EXE"
 $NETFrmWk35SP1x86 = "\\hadbsvc.fhtc.lan\Jenzabar\NET_Frmwk\dotNetFx35setupx86.exe"
 $NETFrmWk35SP1x64 = "\\hadbsvc.fhtc.lan\Jenzabar\NET_Frmwk\dotNetFx35setupx64.exe"
