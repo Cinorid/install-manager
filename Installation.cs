9834faaf54e1cba9dbef0b4954555942a641970a
ddrake <ddrake@7caf2849-a630-4387-b67e-cc3086ce7676>
2011-09-28 21:09:11 +0000

    Made changes to classes and logic flow for the installer.
    git-svn-id: https://projects.fhtc.edu/svn/exscripts/trunk@27 7caf2849-a630-4387-b67e-cc3086ce7676

---
+++ Installation.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using Microsoft.Win32;
+
+namespace SilentInstall {
+
+  public class Installation {
+
+    private XmlNode InstallNode;
+
+    public string Name {
+      get {
+        XmlNode xn = this.InstallNode.SelectSingleNode("./name");
+        string n = xn != null ? xn.InnerText : "";
+        return n;
+      }
+    }
+
+    public string CurrentVersion {
+      get {
+        XmlNode xn = this.InstallNode.SelectSingleNode("./registry");
+        string v = "0.0.0.0";
+
+        if (xn != null) {
+          string subkey = xn.Attributes["latest-subkey"] != null ? xn.Attributes["latest-subkey"].InnerText.Trim() : "false";
+          string kvp = xn.Attributes["value-pair"] != null ? xn.Attributes["value-pair"].InnerText.Trim() : "";
+          string path = xn.InnerText.Trim();
+
+          using (RegistryKey key = Registry.LocalMachine.OpenSubKey(path)) {
+            if (kvp.Length > 0 && subkey == "false") { v = Convert.ToString(key.GetValue(kvp)); }
+            if (kvp.Length == 0 && subkey == "true") { v = key.SubKeyCount > 0 ? key.GetSubKeyNames()[key.SubKeyCount - 1] : "0.0.0.0"; }
+            if (kvp.Length > 0 && subkey == "true") { v = key.SubKeyCount > 0 ? Convert.ToString(key.OpenSubKey((key.GetSubKeyNames()[key.SubKeyCount - 1])).GetValue(kvp)) : "0.0.0.0"; }
+          }
+        }
+
+        return v;
+      }
+    }
+
+    public List<string> Clients {
+      get {
+        List<string> list = new List<string>();
+        XmlNodeList xl = this.InstallNode.SelectNodes("./clients/name");
+
+        foreach (XmlNode n in xl) { list.Add(n.InnerText.Trim().ToUpper()); }
+
+        return list;
+      }
+    }
+
+    public List<InstallationItem> InstallItems {
+      get {
+        List<InstallationItem> list = new List<InstallationItem>();
+        XmlNodeList xl = this.InstallNode.SelectNodes("./install-item");
+
+        foreach (XmlNode n in xl) { list.Add(new InstallationItem(n)); }
+
+        return list;
+      }
+    }
+
+    public Installation(XmlNode InstallNode) {
+      this.InstallNode = InstallNode;
+    }
+  }
+
+  public class InstallationItem {
+
+    private XmlNode ItemNode;
+
+    public string Version {
+      get {
+        XmlNode xn = this.ItemNode.SelectSingleNode("./version");
+        string n = xn != null ? xn.InnerText : "0.0.0.0";
+        return n;
+      }
+    }
+
+    public string CommandType {
+      get {
+        XmlNode xn = this.ItemNode.SelectSingleNode("./command");
+        string n = xn.Attributes["type"] != null ? xn.Attributes["type"].InnerText : "";
+        return n;
+      }
+    }
+
+    public string CommandText {
+      get {
+        XmlNode xn = this.ItemNode.SelectSingleNode("./command");
+        string n = xn != null ? xn.InnerText : "";
+        return n;
+      }
+    }
+
+    public InstallationItem(XmlNode ItemNode) {
+      this.ItemNode = ItemNode;
+    }
+  }
+
+
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,22 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Microsoft.Win32;
-using System.Diagnostics;
-using System.Management.Automation;
 using System.IO;
 using System.Xml;
-using System.Xml.XPath;
 using System.ComponentModel;
 using System.Threading;
 using System.Windows.Threading;
@@ -24,26 +12,41 @@ using System.Windows.Threading;
 namespace SilentInstall {
   public partial class MainWindow : Window {
 
-    private XmlDocument xmlDocument = new XmlDocument();
-    private string ConfigurationPath { get; set; }
+    private XmlDocument xmlDocument = new XmlDocument();   
     private BackgroundWorker installer = new BackgroundWorker();
 
     private List<string> ClientList {
       get {
         List<string> list = new List<string>();
 
-        foreach (XmlNode xnode in this.xmlDocument.DocumentElement.SelectNodes("clients/name")) {
-          list.Add(xnode.InnerText.Trim().ToUpper());
+        XmlDocument xd = new XmlDocument();
+        try {
+          xd.Load(Environment.CurrentDirectory + @"\config\ClientList.xml");
+
+          foreach (XmlNode xnode in xd.DocumentElement.SelectNodes("clients/name")) {
+            list.Add(xnode.InnerText.Trim().ToUpper());
+          }
+        }
+        catch (Exception) {
+          list.Clear();
         }
 
         return list;
       }
     }
+    private string ConfigurationPath {
+      get {
+        StringBuilder sb = new StringBuilder();
+        sb.AppendFormat("{0}", Environment.CurrentDirectory);
+        sb.AppendFormat(@"\config\NT{0}.", Environment.OSVersion.Version.Major.ToString());
+        sb.AppendFormat(@"{0}-config-x{1}.xml", Environment.OSVersion.Version.Minor.ToString(), Environment.Is64BitOperatingSystem ? "64" : "86");
 
+        return sb.ToString();
+      }
+    }
     private string Authentication {
       get { return Convert.ToString(Application.Current.Properties["Authentication"]); }
     }
-
     private string Debug {
       get { return Convert.ToString(Application.Current.Properties["Debug"]); }
     }
@@ -53,18 +56,8 @@ namespace SilentInstall {
 
       installer.DoWork += new DoWorkEventHandler(ThreadedWorker);
 
-      StringBuilder sb = new StringBuilder();
-      sb.AppendFormat("{0}", Environment.CurrentDirectory);
-      sb.AppendFormat(@"\config\NT{0}.", Environment.OSVersion.Version.Major.ToString());
-      sb.AppendFormat(@"{0}-config-x{1}.xml", Environment.OSVersion.Version.Minor.ToString(), Environment.Is64BitOperatingSystem ? "64" : "86");
-
-      this.ConfigurationPath = sb.ToString();
-
       try {
-        using (StreamReader sr = new StreamReader(this.ConfigurationPath)) {
-          this.xmlDocument.LoadXml(sr.ReadToEnd());
-          sr.Close();
-        }
+        this.xmlDocument.Load(this.ConfigurationPath);
       }
       catch (FileNotFoundException) {
         MessageBox.Show("Could not locate config file, make sure the \\config folder is in the same folder as the .exe", "File Not Found");
@@ -79,43 +72,72 @@ namespace SilentInstall {
         Application.Current.Shutdown(2);
       }
 
-      this.installer.RunWorkerAsync(this.xmlDocument);
+      //System.Version v = new System.Version("3.6.7.1");
+      //WriteOutput(v.CompareTo(new System.Version("3.6.7.2")).ToString());
+
+
+      this.installer.RunWorkerAsync();
     }
 
     private void ThreadedWorker(object sender, DoWorkEventArgs e) {
-      this.WriteOutput(this.ConfigurationPath);
+      List<Installation> Installations = new List<Installation>();
 
-      foreach (string s in this.ClientList) {
-        this.WriteOutput(s);
+      foreach (XmlNode n in this.xmlDocument.DocumentElement.SelectNodes("install")) {
+        Installations.Add(new Installation(n));
       }
-      foreach (XmlNode xnode in xmlDocument.DocumentElement.SelectNodes("*")) {
-        if (xnode.SelectSingleNode("./install") != null) {
-          Installation inst = new Installation() { InstallationNode = xnode, Name = xnode.Name };
-          this.WriteOutput(inst.Name);
-          foreach (string s in inst.AllowedMachines) {
-            WriteOutput(s);
-          }
-          WriteOutput(inst.RegistryKey + "|" + inst.RegistryAttribute + "|" + inst.CurrentVersion);
-          foreach (InstallItem i in inst.InstallationItems) {
-            this.WriteOutput(i.Version);
-            foreach (InstallCommand cmd in i.Commands) {
-              if (cmd.Type.ToUpper() == "PS") {
 
+      foreach (Installation i in Installations) {
+        //WriteOutput(String.Format("{0} : {1}", i.Name, i.CurrentVersion));
+        if (i.CurrentVersion == "0.0.0.0") {
+          foreach (InstallationItem item in i.InstallItems) {
+            if (this.Debug == "true") {
+              WriteOutput(String.Format("Installing {0} version {1}...", i.Name, item.Version));
+              Thread.Sleep(5000);
+              WriteOutput(String.Format("{0} version {1} installation finished.", i.Name, item.Version));
+            }
+            else {
+              if (i.Clients.Count > 0 && i.Clients.Contains(Environment.MachineName)) {
+                WriteOutput(String.Format("Installing {0} version {1}...", i.Name, item.Version));
+                Thread.Sleep(5000);
+                WriteOutput(String.Format("{0} version {1} installation finished.", i.Name, item.Version));
               }
               else {
-                using (Process p = new Process()) {
-                  ProcessStartInfo info = new ProcessStartInfo("cmd", "/c " + cmd.Command);
-                  info.UseShellExecute = false;
-                  info.WindowStyle = ProcessWindowStyle.Hidden;
-                  info.WorkingDirectory = AppDomain.CurrentDomain.BaseDirectory;
-                  p.StartInfo = info;
-                  //p.Start();
+                if (this.ClientList.Contains(Environment.MachineName)) {
+                  WriteOutput(String.Format("Installing {0} version {1}...", i.Name, item.Version));
+                  Thread.Sleep(5000);
+                  WriteOutput(String.Format("{0} version {1} installation finished.", i.Name, item.Version));
                 }
-              }             
+              }
+            }
+          }
+        }
+        else {
+          foreach (InstallationItem item in i.InstallItems) {
+            if (new Version(i.CurrentVersion).CompareTo(new Version(item.Version)) == -1) {
+              if (this.Debug == "true") {
+                WriteOutput(String.Format("Installing {0} version {1}...", i.Name, item.Version));
+                Thread.Sleep(5000);
+                WriteOutput(String.Format("{0} version {1} installation finished.", i.Name, item.Version));
+              }
+              else {
+                if (i.Clients.Count > 0 && i.Clients.Contains(Environment.MachineName)) {
+                  WriteOutput(String.Format("Installing {0} version {1}...", i.Name, item.Version));
+                  Thread.Sleep(5000);
+                  WriteOutput(String.Format("{0} version {1} installation finished.", i.Name, item.Version));
+                }
+                else {
+                  if (this.ClientList.Contains(Environment.MachineName)) {
+                    WriteOutput(String.Format("Installing {0} version {1}...", i.Name, item.Version));
+                    Thread.Sleep(5000);
+                    WriteOutput(String.Format("{0} version {1} installation finished.", i.Name, item.Version));
+                  }
+                }
+              }
             }
           }
         }
       }
+
     }
 
     private void WriteOutput(string message) {
@@ -129,107 +151,5 @@ namespace SilentInstall {
       }
     }
 
-  }
-
-  public class Installation {
-
-    public XmlNode InstallationNode { get; set; }
-    public string Name { get; set; }
-    public string RegistryKey {
-      get {
-        if (this.InstallationNode.SelectSingleNode("./registry") != null) {
-          return this.InstallationNode.SelectSingleNode("./registry").InnerText;
-        }
-        else {
-          return "";
-        }
-      }
-    }
-
-    public string RegistryAttribute {
-      get {
-        if (this.InstallationNode.SelectSingleNode("./registry") != null) {
-          return this.InstallationNode.SelectSingleNode("./registry").Attributes["property"].InnerText;
-        }
-        else {
-          return "";
-        }
-      }
-    }
-
-    public string CurrentVersion {
-      get {
-        string version = "";
-
-        if (!String.IsNullOrEmpty(this.RegistryKey)) {
-          if (!String.IsNullOrEmpty(this.RegistryAttribute)) {
-            using (RegistryKey key = Registry.LocalMachine.OpenSubKey(this.RegistryKey)) {
-              version = Convert.ToString(key.GetValue(this.RegistryAttribute));
-            }
-          }
-          else {
-            using (RegistryKey key = Registry.LocalMachine.OpenSubKey(this.RegistryKey)) {
-              version = key.SubKeyCount > 0 ? key.GetSubKeyNames()[key.SubKeyCount - 1] : "";
-            }
-          }
-        }
-
-        return version.Trim();
-      }
-
-    }
-
-    public List<InstallItem> InstallationItems {
-      get {
-        List<InstallItem> list = new List<InstallItem>();
-        foreach (XmlNode n in this.InstallationNode.SelectNodes("./install")) {
-          list.Add(new InstallItem() { Version = n.SelectSingleNode("./version").InnerText, ItemNode = n });
-        }
-        return list;
-      }
-    }
-
-    public List<string> AllowedMachines {
-      get {
-        List<string> list = new List<string>();
-        foreach (XmlNode n in this.InstallationNode.SelectNodes("./allowed-machines/name")) {
-          list.Add(n.InnerText);
-        }
-        return list;
-      }
-    }
-
-    public Installation() {
-
-    }
-
-  }
-
-  public class InstallItem {
-
-    public XmlNode ItemNode { get; set; }
-    public string Version { get; set; }
-    public List<InstallCommand> Commands {
-      get {
-        List<InstallCommand> list = new List<InstallCommand>();
-        foreach (XmlNode n in this.ItemNode.SelectNodes("./command")) {
-          list.Add(new InstallCommand() { Type = n.Attributes["type"].InnerText, Command = n.InnerText });
-        }
-        return list;
-      }
-    }
-
-    public InstallItem() {
-
-    }
-  }
-
-  public class InstallCommand {
-    public string Type { get; set; }
-    public string Command { get; set; }
-
-    public InstallCommand() {
-
-    }
   }
 }
